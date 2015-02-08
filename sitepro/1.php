c412489c692d8764fc73702b23b6ddea5f64936f
Shane Gray <shanedgray@gmail.com>
2015-02-08 22:14:06 +0000

    First commit

---
+++ sitepro/1.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Homepage</title>
+	<base href="{{base_url}}" />
+	<meta name="viewport" content="width=1003" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.8.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+	<style type="text/css">
+		body { background: #ffffff none repeat-x scroll center top; }
+		.wb_sbg { background: transparent none repeat-x scroll left top; }
+		.wb-stl-pagetitle { font: normal bold 200px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #ffffff; line-height: 38px; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; text-transform: none; }
+		.wb-stl-pagetitle a { font: normal bold 200px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 38px; font-weight: normal; font-style: normal; }
+		.wb-stl-pagetitle a:hover { font: normal bold 200px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 38px; font-weight: normal; font-style: normal; }
+		.wb-stl-pagetitle ul { list-style-image: url('null'); }
+		.wb-stl-subtitle { font: normal bold 72px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 64px; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; text-transform: none; }
+		.wb-stl-subtitle a { font: normal bold 72px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 64px; font-weight: normal; font-style: normal; }
+		.wb-stl-subtitle a:hover { font: normal bold 72px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 64px; font-weight: normal; font-style: normal; }
+		.wb-stl-subtitle ul { list-style-image: url('null'); }
+		.wb-stl-heading1 { font: normal bold 44px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; text-transform: capitalize; }
+		.wb-stl-heading1 a { font: normal bold 44px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; font-weight: normal; font-style: normal; }
+		.wb-stl-heading1 a:hover { font: normal bold 44px Arial,Helvetica,sans-serif; text-align: left; text-decoration: underline; color: #000000; line-height: normal; font-weight: normal; font-style: normal; }
+		.wb-stl-heading1 ul { list-style-image: url('null'); }
+		.wb-stl-heading2 { font: normal bold 24px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; }
+		.wb-stl-heading2 a { font: normal bold 24px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; font-weight: normal; font-style: normal; }
+		.wb-stl-heading2 a:hover { font: normal bold 24px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; font-weight: normal; font-style: normal; }
+		.wb-stl-heading2 ul { list-style-image: url('null'); }
+		.wb-stl-heading3 { font: normal bold 42px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #ffffff; line-height: normal; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; }
+		.wb-stl-heading3 a { font: normal bold 42px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; font-weight: normal; font-style: normal; }
+		.wb-stl-heading3 a:hover { font: normal bold 42px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: normal; font-weight: normal; font-style: normal; }
+		.wb-stl-heading3 ul { list-style-image: url('null'); }
+		.wb-stl-normal { font: normal normal 13px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #474747; line-height: 26px; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; }
+		.wb-stl-normal a { font: normal normal 13px Arial,Helvetica,sans-serif; text-align: left; text-decoration: underline; color: #1b669f; line-height: 26px; font-weight: normal; font-style: normal; }
+		.wb-stl-normal a:hover { font: normal normal 13px Arial,Helvetica,sans-serif; text-align: left; text-decoration: underline; color: #000000; line-height: 26px; font-weight: normal; font-style: normal; }
+		.wb-stl-normal ul { list-style-image: url('null'); }
+		.wb-stl-highlight { font: normal normal 16px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 24px; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; }
+		.wb-stl-highlight a { font: normal normal 16px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 24px; font-weight: normal; font-style: normal; }
+		.wb-stl-highlight a:hover { font: normal normal 16px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 24px; font-weight: normal; font-style: normal; }
+		.wb-stl-highlight ul { list-style-image: url('null'); }
+		.wb-stl-special { font: normal bold 18px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 28px; margin: 0px 0px 20px 0px; padding: 0px 0px 0px 0px; }
+		.wb-stl-special a { font: normal bold 18px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 28px; font-weight: normal; font-style: normal; }
+		.wb-stl-special a:hover { font: normal bold 18px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 28px; font-weight: normal; font-style: normal; }
+		.wb-stl-special ul { list-style-image: url('null'); }
+		.wb-stl-footer { font: normal normal 13px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #000000; line-height: 20px; margin: 0px 0px 0px 0px; padding: 0px 0px 0px 0px; }
+		.wb-stl-footer a { font: normal normal 13px Arial,Helvetica,sans-serif; text-align: left; text-decoration: underline; color: #1b669f; line-height: 20px; font-weight: normal; font-style: normal; }
+		.wb-stl-footer a:hover { font: normal normal 13px Arial,Helvetica,sans-serif; text-align: left; text-decoration: none; color: #080808; line-height: 20px; font-weight: normal; font-style: normal; }
+		.wb-stl-footer ul { list-style-image: url('null'); }
+		body, .wb_sbg { min-width: 1003px; }
+		#wb_element_instance0 ul { background: transparent none repeat scroll left top; border: 0px none #000000; text-align: left; }
+		#wb_element_instance0 ul ul { background: #2e2e2e none repeat scroll left top; }
+		#wb_element_instance0 li { margin: 10px 10px 10px 10px; }
+		#wb_element_instance0 li a { text-transform: none; border: 0px none #000000; padding: 2px 2px 2px 2px; font: normal normal 22px Arial,Helvetica,sans-serif; text-decoration: none; color: #2c2c2c; line-height: 24px; background: transparent none repeat scroll left top; }
+		#wb_element_instance0 li:hover > a, #wb_element_instance0 li:focus > a { border: 0px none #000000; font: normal normal 22px Arial,Helvetica,sans-serif; text-decoration: none; color: #43852d; line-height: 24px; background: transparent none repeat scroll left top; }
+		#wb_element_instance0 li.active > a { border: 0px none #000000; font: normal normal 22px Arial,Helvetica,sans-serif; text-decoration: none; color: #43852d; line-height: 24px; background: transparent none repeat scroll left top; }
+	</style>
+	<link href="css/site.css?v=1.0.4" rel="stylesheet" type="text/css" />
+		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root" style="width: 1003px; height: 731px;"><div class="vbox wb_container" id="wb_header" style="height: 511px; background: transparent none repeat scroll left top;">
+	
+<div id="wb_element_instance1" class="wb_element" style="left: 310px; top: 110px; width: 380px; height: 380px; min-width: 380px; min-height: 380px; z-index: 58;"><img alt="" src="gallery/b2018b0894e5272bbd07111ea4454d05_380x380.png" style="width: 380px; height: 380px;"></div></div>
+<div class="vbox wb_container" id="wb_main" style="height: 70px; background: transparent none repeat scroll left top; padding: 0 0 50px 0;">
+	
+<div id="wb_element_instance2" class="wb_element" style="left: 520px; top: 40px; width: 30px; height: 30px; min-width: 30px; min-height: 30px; z-index: 59;"><a href="http://twitter.com/egaliohq" target="1"><img alt="Egalio HQ" src="gallery/26f50ce92b7e63ea6050a3fbedead192_30x30.png" style="width: 30px; height: 30px;"></a></div><div id="wb_element_instance3" class="wb_element" style="left: 450px; top: 40px; width: 30px; height: 30px; min-width: 30px; min-height: 30px; z-index: 61;"><a href="http://blog.egalio.com" title="Egalio's Blog" target="1"><img alt="Egalio's Blog" src="gallery/a90f95c66684353db1403def18ca362d_30x30.png" style="width: 30px; height: 30px;"></a></div><div id="wb_element_instance6" class="wb_element" style="left: 0px; top: 90px; min-width: 0px; min-height: 0px; z-index: 9000; width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(1);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance6");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance6").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 180px; background: transparent none repeat scroll center bottom;">
+	
+<div id="wb_element_instance0" class="wb_element" style="left: 710px; top: 20px; width: 280px; height: 57px; min-width: 280px; min-height: 57px; z-index: 39;"><ul class="hmenu"></ul></div><div id="wb_element_instance4" class="wb_element" style="left: 240px; top: 30px; width: 510px; height: 27px; min-width: 510px; min-height: 27px; z-index: 40;  line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><font color="#000000" size="3"><span style="line-height: 24px;">Egalio Ltd Registered in Ireland</span></font></p>
+</div><div id="wb_element_instance5" class="wb_element" style="left: 76px; top: 0px; width: 200px; height: 100px; min-width: 200px; min-height: 100px; z-index: 41; overflow: hidden;"><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52836717-1', 'auto');
+  ga('send', 'pageview');
+
+</script></div><div id="wb_element_instance7" class="wb_element" style="left: 0px; top: 120px; min-width: 0px; min-height: 0px; z-index: 9999; text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.height(100);
+				}
+			});
+			</script></div></div><div class="wb_sbg" style="min-height: 731px;"></div></div></body>
+</html>
---
+++ sitepro/ga.php
@@ -0,0 +1,17 @@
+<?php //0046a
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+?>
+HR+cPo4oyueQ6iYjOebcgN/M72yV23gFHjbRjSPUQRwNCQCiGQvhWAPkY1M2Hi0YtHmdM5iW/4iO
+oUUC3NOXdgdmbNIhmP0REqxPJ1iig4G7ekMphSWdRfzLy+RDe6E2W93cA7S7JQ8JvDQW2brAr1YY
+AsD5qxCayYggzhVQcfAFc/0LJ9nQjKOqu33oDi7G4dOHSKj52rBnYCLkC52MvPyq0xSSy23PslKO
+SsaXHDzUKPwBZn5spVCjOT3fek0Y88R7AkveLsKnebWbmZ99HbX/wP1MPvxOMbW5HDA5UGB2O4CQ
+17k9/KLFalaviKatznurshlcHu3eNZK0brgJPiK5T1FL815BolRdZGTvPk8s+7VAmpBf2eNuw8Ga
+t8J5yWpckCku5jG4Js6lS12Mn2sR0xGo7LTvkltpFJEYPs4jXewc2u4mREgUHie4Z0RNxsI8UA2e
+Uvkoex6GDx8Qqm6mFHTPGpIeL7DW7DFgKyTH0qmEs1887A48FGH2x/Tb7//4hUmd5BFTvE8hWMVu
+jYkQ6vgZmvjXkpxuxyQJA5kgmOxjR0/GDrf87D4MnPB1llu8c1+pN6YmvUG1fhcVSgAXpQlE55xq
+UPhx/9MefajQQxAaBW55vMC/i/Kbb0SETiEh6YWhfXa8qAsrkpSPxmGbJv7dhdVxo/o3SeG5Qwfj
+CvA4K1UkoBhkRW7lksXkXXW/CMsfEa/RtwvhHqWkNJRZKla6QZrprqOgLU6aMa3lAbOdfWE/1yir
+K6B/FVh7qMkvD3ipGZepIRMUPWPod1gQxiFK9KYEKdqKrwvlOLC5UTHtQhT1hK/J83NzoUHjJAOV
+Hy0jfCZ7KV+6f8DMmnbEQdssvsbxSkgNxDsSFMJNGy1sAmBQEaX37wlaC19HpIkr22t+l4+dlpct
+JrQQ/ocWJF5LYItDvpCkHR+Db3r8+4N/ic0nnHLEElg6jqS9axPzZEQ8LhOmA3gs0ovgCqi4n+Zp
+ivWAgkReznV+aRXxpNb7HsfXtLrh2pFkhy3fiT/kcFJjRBb0fahG/JS=
